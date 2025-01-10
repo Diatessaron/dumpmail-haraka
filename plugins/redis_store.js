27a4b525f7d3dee225e2e80074bce262a27a035f
Azat Fazlyev <afazlyev436@gmail.com>
2025-01-10 16:11:42 +0300

    Added redis user

--- plugins/redis_store.js
+++ plugins/redis_store.js
@@ -7,6 +7,8 @@ require('dotenv').config();
 let redis = new Redis({
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
+    username: process.env.REDIS_USER,
+    password: process.env.REDIS_PASSWORD,
     lazyConnect: true
 });
 
